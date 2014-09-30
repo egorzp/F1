66d4a8fe39bfbac00de4c7439220d9c4c857152a
egorzp <romanenkoee@gmail.com>
2014-09-30 15:02:44 +0300

    changes on work

---
+++ src/Enemy.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by egor on 30.09.2014.
+ */
+public class Enemy {
+    int x;
+    int y;
+    int v;
+    Image img = new ImageIcon("res/Enemy.png").getImage();
+
+}
